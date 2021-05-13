a24f2a96ea8dc8bd518deaafef8a8ddf2286bd11
Leandro <fahurleandro@gmail.com>
2021-05-13 02:27:20 -0700

    JSON fix

--- models/product.js
+++ models/product.js
@@ -1,3 +1,58 @@
+// const fs = require("fs");
+// const path = require("path");
+
+// const getProductsFromFile = (cb) => {
+//   const p = path.join(
+//     path.dirname(process.mainModule.filename),
+//     "data",
+//     "products.json"
+//   );
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     }
+//     cb(JSON.parse(fileContent));
+//   });
+// };
+
+// module.exports = class Product {
+//   constructor(t) {
+//     this.title = t;
+//   }
+
+//   save() {
+//     getProductsFromFile((products) => {
+//       fs.readFile(p, (err, fileContent) => {});
+
+//       products.push(this);
+//       fs.writeFile(p, JSON.stringify(products), (err) => {
+//         console.log(err);
+//       });
+//     });
+//   }
+
+//   static fetchAll(cb) {
+//     getProductsFromFile(cb);
+//   }
+
+//   //   static fetchAll(cb) {
+//   //     const p = path.join(
+//   //       path.dirname(process.mainModule.filename),
+//   //       "data",
+//   //       "products.json"
+//   //     );
+//   //     fs.readFile(p, (err, fileContent) => {
+//   //       if (err) {
+//   //         cb([]);
+//   //       } else if (fileContent.length) {
+//   //         cb(JSON.parse(fileContent));
+//   //       } else {
+//   //         cb([]);
+//   //       }
+//   //     });
+//   //   }
+// };
+
 const fs = require("fs");
 const path = require("path");
 
@@ -32,26 +87,9 @@ module.exports = class Product {
     );
     fs.readFile(p, (err, fileContent) => {
       if (err) {
-        cb([]);
+        return cb([]);
       }
       cb(JSON.parse(fileContent));
     });
   }
-
-  //   static fetchAll(cb) {
-  //     const p = path.join(
-  //       path.dirname(process.mainModule.filename),
-  //       "data",
-  //       "products.json"
-  //     );
-  //     fs.readFile(p, (err, fileContent) => {
-  //       if (err) {
-  //         cb([]);
-  //       } else if (fileContent.length) {
-  //         cb(JSON.parse(fileContent));
-  //       } else {
-  //         cb([]);
-  //       }
-  //     });
-  //   }
 };
